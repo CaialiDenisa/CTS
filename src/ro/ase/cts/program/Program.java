afe7a763af5d6fcc6e4ef16ef77b0d7a7af36356
CaialiDenisa <caialidenisa18@stud.ase.ro>
2021-03-04 10:22:57 +0200

    commit

--- src/ro/ase/cts/program/Program.java
+++ src/ro/ase/cts/program/Program.java
@@ -2,14 +2,16 @@ package ro.ase.cts.program;
 
 import java.io.FileNotFoundException;
 import java.util.List;
-import ro.ase.cts.clase.*;
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.readers.AngajatReader;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		List<Angajat> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
 			for(Angajat angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
